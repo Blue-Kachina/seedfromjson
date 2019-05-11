4ba8ab1cf1c800f92095f9976588a2ce0b3af6a7
Blue-Kachina <matt_leering@hotmail.com>
2019-05-10 23:29:19 -0400

    Attempting to set everything up for Composer usage

---
+++ src/SeedFromJSONProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace bluekachina\seedfromjson;
+
+use Illuminate\Support\ServiceProvider;
+
+class SeedFromJSONProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->make('bluekachina\seedfromjson\SeedFromJSON.php');
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes.php');
+        $this->loadMigrationsFrom(__DIR__.'/migrations');
+        $this->loadViewsFrom(__DIR__.'/views', 'seedfromjson');
+        $this->publishes([
+            __DIR__.'/views' => base_path('resources/views/bluekachina/seedfromjson'),
+        ]);
+    }
+}
